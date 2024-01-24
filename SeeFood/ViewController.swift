6b0e386a9fbc5c5dcce367efec52fd48e981bbcc
jhammad <javier.hammad@gmail.com>
2024-01-24 12:46:43 +0000

    Getting image recognition back from our V3 Model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
-import CoreML
-import Vision
+import CoreML // import CoreML
+import Vision // image analysis request
 
 // simulation won't work will need a physical device to test and access camera
 
@@ -26,11 +26,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             // assign the image to the valueimageView.image
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            //launch the func detect
+            detect(image: ciimage)
         }
         // dismiss the image picker
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
+    // This function is responsible for using a Core ML model (InceptionV3) to detect contours in a given CIImage.
+    func detect(image: CIImage) {
+        // Attempt to create a Core ML model instance using the InceptionV3 model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            // If the model creation fails, print an error message and terminate the app
+            fatalError("Could not load the MLModel InceptionV3")
+        }
+        // Create a request for the Vision framework using the Core ML model
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            // Completion block: This is executed after the request is processed
+            // Ensure that the results can be cast to an array of VNContoursObservation
+            guard let results = request.results as? [VNContoursObservation] else {
+                // If casting fails, print an error message and terminate the app
+                fatalError("Could not extract results from the request for processing")
+            }
+            // Print the detected contours (results) to the console
+            print(results)
+        }
+        // Create an image request handler for processing the CIImage
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            // Perform the image request using the created handler and request
+            try handler.perform([request])
+        } catch {
+            // If an error occurs during the request, print the error message to the console
+            print(error)
+        }
+    }
+        
     // This is a function that gets called when the camera button is tapped
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         // We use the 'present' function to display the imagePicker
